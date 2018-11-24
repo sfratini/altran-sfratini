19f74c5d4717e4545c236ea2bb4a87b42e6e09b1
Sebastian Fratini <sebastian.fratini@gmail.com>
2018-11-24 15:48:28 -0300

    Card

--- altran-app/src/components/UserCard.js
+++ altran-app/src/components/UserCard.js
@@ -21,6 +21,7 @@ import CloseIcon from '@material-ui/icons/Close';
 const styles = theme => ({
   card: {
     maxWidth: 400,
+    minWidth: 400
   },
   media: {
     height: 0,
@@ -48,10 +49,14 @@ const styles = theme => ({
 });
 
 class UserCard extends React.Component {
-  state = { expanded: false };
+  state = { expanded: false, workExpanded: false };
 
   handleExpandClick = () => {
     this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+  handleExpandClickWork = () => {
+    this.setState(state => ({ workExpanded: !state.workExpanded }));
   };
 
   onClose = () => {
@@ -88,15 +93,13 @@ class UserCard extends React.Component {
         <Typography component="p">Height: {item.height}</Typography>
         <Typography component="p">Hair Color: {item.hair_color}</Typography>
         </CardContent>
+      
+      
         <CardActions className={classes.actions} disableActionSpacing>
-          <IconButton aria-label="Add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="Share">
-            <ShareIcon />
-          </IconButton>
-          <IconButton
-            className={classnames(classes.expand, {
+
+        <Typography component="p">Friends</Typography>
+
+          <IconButton className={classnames(classes.expand, {
               [classes.expandOpen]: this.state.expanded,
             })}
             onClick={this.handleExpandClick}
@@ -106,31 +109,31 @@ class UserCard extends React.Component {
             <ExpandMoreIcon />
           </IconButton>
         </CardActions>
+
         <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
           <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-              minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-              heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-              browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-              chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-              salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-              minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-              without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
-              to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
-              cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
-              minutes more. (Discard any mussels that don’t open.)
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
+            <Typography paragraph>{item.friends.join(", ")}</Typography>
+          </CardContent>
+        </Collapse>
+
+         <CardActions className={classes.actions} disableActionSpacing>
+
+            <Typography component="p">Professions</Typography>
+
+            <IconButton className={classnames(classes.expand, {
+                [classes.expandOpen]: this.state.workExpanded,
+                })}
+                onClick={this.handleExpandClickWork}
+                aria-expanded={this.state.workExpanded}
+                aria-label="Show more"
+            >
+                <ExpandMoreIcon />
+            </IconButton>
+            </CardActions>
+
+         <Collapse in={this.state.workExpanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>{item.professions.join(", ")}</Typography>
           </CardContent>
         </Collapse>
       </Card>
